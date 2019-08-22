6e234ae2c2adeda9130d484d0d5ead0ad23df31a
Matthew Ream <matthewtimothyream@gmail.com>
2019-08-22 16:51:15 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+def badge_maker(element)
+  "Hello, my name is #{element}."
+end
+
+  def batch_badge_creator(array)
+    batch = []
+  array.each do |name|
+    batch << "Hello, my name is #{name}."
+   end 
+   return batch
+  end
+  
+  def assign_rooms(array)
+  speakers_assignments = []
+  array.each_with_index do |name, index| 
+    speakers_assignments << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+end
+return speakers_assignments
+end
+
+def printer(names)
+batch = batch_badge_creator(names)
+batch.each do |badge|
+  puts badge
+end
+speakers_assignments = assign_rooms(names)
+speakers_assignments.each do |assignment|
+  puts assignment
+end
+end
