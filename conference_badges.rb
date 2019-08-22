c1a32d9d9821ac2b5ae257be30f95572fb3e81e1
Matthew Ream <matthewtimothyream@gmail.com>
2019-08-22 16:12:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+def badge_maker
+  def batch_badge_creator(array)
+     batch = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  array.each do |name|
+    puts "Hello, my name is #{name}."
+   end 
+  end
+end
+  
+  #def assign_rooms(array, array_location)
+  #batch = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  #location = [1, 2, 3, 4, 5, 6, 7]
+  #speakers_assignments = []
+  #array.each do |name| && array.each_with_index do |room_number|
+  #puts "Hello, #{name}! You'll be assigned to room #{room_number}!
+  #speakers_assignments << each_with_index
+#end
+#end
+#return speakers_assignments
+#end
